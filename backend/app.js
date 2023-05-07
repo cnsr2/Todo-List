09d32e5bfd507c06cedc24eab79ad6e65b89d502
Emre Can <cansarrr2@gmail.com>
2023-05-08 02:02:19 +0300

    fix methods

--- backend/app.js
+++ backend/app.js
@@ -16,45 +16,45 @@ mongoose.connect(process.env.CONNECTION_STRING);
 
 // models
 const TodoSchema = new Schema({
- text: String,
- isDone: Boolean,
- dateCreated: {
-  type: Date,
-  default: Date.now,
- },
+    text: String,
+    isDone: Boolean,
+    dateCreated: {
+        type: Date,
+        default: Date.now,
+    },
 });
 const Todo = mongoose.model('Todo', TodoSchema);
 
 //Routes
 
 app.get('/', async (req, res) => {
- const nd = await Todo.find({});
- res.send(nd);
+    const nd = await Todo.find({});
+    res.send(nd);
 });
 app.post('/addtodo', async (req, res) => {
- console.log(req.body);
- await Todo.create(req.body);
- res.status(200);
+    console.log(req.body);
+    await Todo.create(req.body);
+    res.status(200);
 });
-app.post('/changetodo', async (req, res) => {
- const { text, isDone } = req.body;
- try {
-  const result = await Todo.updateOne(
-   { _id: req.body.id },
-   { $set: { text: text, isDone: isDone } },
-  );
-  console.log('Güncelleme başarılı:', result);
- } catch (error) {
-  console.error('Güncelleme hatası:', error);
- }
+app.put('/changetodo', async (req, res) => {
+    const { text, isDone } = req.body;
+    try {
+        const result = await Todo.updateOne(
+            { _id: req.body.id },
+            { $set: { text: text, isDone: isDone } },
+        );
+        console.log('Güncelleme başarılı:', result);
+    } catch (error) {
+        console.error('Güncelleme hatası:', error);
+    }
 });
-app.post('/deletetodo/:id', async (req, res) => {
- console.log(req.params.id);
- const id = req.params.id;
- await Todo.deleteOne({ _id: id });
- res.status(200);
+app.delete('/deletetodo/:id', async (req, res) => {
+    console.log(req.params.id);
+    const id = req.params.id;
+    await Todo.deleteOne({ _id: id });
+    res.status(200);
 });
 
 app.listen(5500, () => {
- console.log('Server listening on port 5500');
+    console.log('Server listening on port 5500');
 });
--- frontend/app.js
+++ frontend/app.js
@@ -1,66 +1,66 @@
 const TODO_LIST = {
- todoList: [],
- todo: {},
- selectedTodo: '',
+  todoList: [],
+  todo: {},
+  selectedTodo: '',
 };
 class Todo {
- constructor(text) {
-  this.text = text;
-  this.isDone = false;
- }
+  constructor(text) {
+    this.text = text;
+    this.isDone = false;
+  }
 }
 getTodoList();
 
 async function getTodoList() {
- const RESPONSE = await fetch('http://localhost:5500/');
- const DATA = await RESPONSE.json();
- TODO_LIST.todoList = DATA;
- screen();
+  const RESPONSE = await fetch('http://localhost:5500/');
+  const DATA = await RESPONSE.json();
+  TODO_LIST.todoList = DATA;
+  screen();
 }
 
 async function addTodo() {
- await fetch('http://localhost:5500/addtodo', {
-  method: 'POST',
-  headers: {
-   'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(TODO_LIST.todo),
- });
- TODO_LIST.todo = {};
+  await fetch('http://localhost:5500/addtodo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(TODO_LIST.todo),
+  });
+  TODO_LIST.todo = {};
 }
-async function deleteTodo() {
- console.log(TODO_LIST.selectedTodo);
- await fetch(`http://localhost:5500/deletetodo/${TODO_LIST.selectedTodo}`, {
-  method: 'DELETE',
- });
- TODO_LIST.selectedTodo = '';
+async function deleteTodo(id) {
+  console.log(TODO_LIST.selectedTodo);
+  await fetch(`http://localhost:5500/deletetodo/${id}`, {
+    method: 'POST',
+  });
+  //  TODO_LIST.selectedTodo = '';
 }
 
 form.addEventListener('submit', async (e) => {
- e.preventDefault();
- const todo = new Todo(todoInput.value);
- TODO_LIST.todo = todo;
- addTodo();
- setTimeout(() => {
-  getTodoList();
- }, 1000);
+  e.preventDefault();
+  const todo = new Todo(todoInput.value);
+  TODO_LIST.todo = todo;
+  addTodo();
+  setTimeout(() => {
+    getTodoList();
+  }, 200);
 });
 
 mainUL.addEventListener('click', (e) => {
- TODO_LIST.selectedTodo = e.target.parentElement.id;
- deleteTodo();
- setTimeout(() => {
-  getTodoList();
- }, 1000);
+  //  TODO_LIST.selectedTodo = e.target.parentElement.id;
+  deleteTodo(e.target.parentElement.id);
+  setTimeout(() => {
+    getTodoList();
+  }, 200);
 });
 
 function screen() {
- console.log(TODO_LIST.todoList);
- mainUL.innerHTML = TODO_LIST.todoList
-  .map((todo) => {
-   return `
+  console.log(TODO_LIST.todoList);
+  mainUL.innerHTML = TODO_LIST.todoList
+    .map((todo) => {
+      return `
   <li id="${todo._id}"> ${todo.text} <span> ${todo.isDone} </span> <a> X </a> </li>
   `;
-  })
-  .join('');
+    })
+    .join('');
 }
